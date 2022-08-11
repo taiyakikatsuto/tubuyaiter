c1ab1fe77181627946b11655e097dd1ae76ff552
taiyakikatsuto <super.katsuto@gmail.com>
2022-08-11 12:58:45 +0900

    create:controller,migration,model,seedder

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\ImageController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TubuyakiController;
+use App\Http\Controllers\TubuyakiListController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+Route::apiResource('tubuyaki', TubuyakiController::class);
+
+Route::prefix('tubuyaki')->name('tubuyaki.')->group(function() {
+    Route::get('/list',  TubuyakiListController::class)->name('list');
 });
+
+Route::prefix('image')->name('image.')->controller(ImageController::class)->group(function(){
+    Route::post('/upload',  'upload')->name('upload');
+    Route::get('/show',  'show')->name('show');
+    Route::get('/download',  'download')->name('download');
+});
+
+
